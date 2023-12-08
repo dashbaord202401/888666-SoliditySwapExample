8104771543d3477e21028beb49d83203eee30e52
GuiFlam <gui.flamand@gmail.com>
2023-12-08 14:17:03 -0500

    Modifications

--- Swap_Example.sol
+++ Swap_Example.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 interface IRouter {
     function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);
 }
@@ -17,21 +14,20 @@ interface IERC20 {
 contract Swap_Example {
 
     address private router; // Swap Router Address
-    address private wavaxAddress; // Address of WAVAX
 
     constructor(address _router, address _wavaxAddress){
         router = _router;
-        wavaxAddress = _wavaxAddress;
     }
 
     receive() external payable {}
 
-    function swap(address from, uint256 amountOutMin, address[] calldata path, uint256 amountIn, address tokenIn, uint256 deadline, uint256 gasEstimate) external {
-        // We transfer the wavax from the user (msg.sender) to this contract.
-        IERC20(tokenIn).transferFrom(from, address(this), amountIn);
+    // For this function to be used, the amountIn of tokenIn needs to already be approved to be spent by the address of this contract, or else it won't work
+    function swap(uint256 amountOutMin, address[] calldata path, uint256 amountIn, address tokenIn, uint256 deadline) external {
+        // We transfer the tokenIn from the user (msg.sender) to this contract.
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
         // We approve the router as a spender for our Wavax.
         IERC20(tokenIn).approve(router, amountIn);
         // We do the swap using the router.
-        IRouter(router).swapExactTokensForTokens(amountIn, amountOutMin, path, from, deadline);
+        IRouter(router).swapExactTokensForTokens(amountIn, amountOutMin, path, msg.sender, deadline);
     }
 }
